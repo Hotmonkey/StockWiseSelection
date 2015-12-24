752ede166b240b553426f2329ed49481d6e1a8bf
Hotmonkey <cky1991@163.com>
2015-12-25 01:03:31 +0800

    first commit

---
+++ StockWiseSelection/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace StockWiseSelection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 32);
+            this.textBox1.MaxLength = 8;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "起始日期";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "结束日期";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(106, 65);
+            this.textBox2.MaxLength = 8;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(134, 21);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "下跌天数";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(106, 99);
+            this.textBox3.MaxLength = 5;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(134, 21);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "筛选";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(43, 179);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(197, 183);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(105, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "导出";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 426);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "智能选股";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ StockWiseSelection/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace StockWiseSelection
+{
+    public delegate void ScreenCallback(List<string> result);
+    public partial class Form1 : Form
+    {
+        List<string> ScreenResult;
+        Thread tGetStock;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tGetStock != null && tGetStock.ThreadState == ThreadState.Background)
+            {
+                tGetStock.Abort();
+            }
+            StockAction sa = new StockAction();
+            tGetStock = new Thread(new ParameterizedThreadStart(sa.StockScreen));
+            tGetStock.IsBackground = true;
+            StockScreenArgs ssa;
+            try
+            {
+                ssa = new StockScreenArgs(this.textBox1.Text, this.textBox2.Text, int.Parse(this.textBox3.Text), this.textBox4, ScreenDone);
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show("输入有误，请重新输入！");
+                return;
+            }
+            tGetStock.Start(ssa);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tGetStock.ThreadState == ThreadState.Background)
+            {
+                MessageBox.Show("筛选未完成！");
+            }
+            else if (ScreenResult == null)
+            {
+                MessageBox.Show("未筛选数据！");
+            }
+            else
+            {
+                MessageBox.Show("这里要显示保存的对话框");
+            }
+        }
+
+        private void ScreenDone(List<string> result)
+        {
+            this.ScreenResult = result;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsNumber(e.KeyChar) || e.KeyChar == 8))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsNumber(e.KeyChar) || e.KeyChar == 8))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsNumber(e.KeyChar) || e.KeyChar == 8))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+
+    class StockScreenArgs
+    {
+        string format = "yyyyMMdd";
+
+        string beginDate;
+        string endDate;
+        int declineDayCount;
+        TextBox resultTb;
+        ScreenCallback sCallback;
+
+        public string BeginDate { get { return beginDate; } }
+        public string EndDate { get { return endDate; } }
+        public int DeclineDayCount { get { return declineDayCount; } }
+        public TextBox ResultTb { get { return resultTb; } }
+        public ScreenCallback SCallback { get { return sCallback; } }
+
+        public StockScreenArgs()
+        {
+ 
+        }
+
+        public StockScreenArgs(string beginDate, string endDate, int declineDayCount, TextBox tb, ScreenCallback sCallback)
+        {
+            DateTime.ParseExact(beginDate, format, System.Globalization.CultureInfo.InvariantCulture);
+            DateTime.ParseExact(endDate, format, System.Globalization.CultureInfo.InvariantCulture);
+            this.beginDate = beginDate;
+            this.endDate = endDate;
+            this.declineDayCount = declineDayCount;
+            this.resultTb = tb;
+            this.sCallback = sCallback;
+        }
+
+        public void Settings(string beginDate, string endDate, int declineDayCount, TextBox tb, ScreenCallback sCallback)
+        {
+            this.beginDate = beginDate;
+            this.endDate = endDate;
+            this.declineDayCount = declineDayCount;
+            this.resultTb = tb;
+            this.sCallback = sCallback;
+        }
+    }
+
+    class StockAction
+    {
+        /// <summary>
+        /// http获取数据
+        /// </summary>
+        /// <param name="Url"></param>
+        /// <returns></returns>
+        private string HttpGet(string Url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            request.Method = "GET";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream);
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+
+        private List<string> SplitOriginStr(string origin)
+        {
+            List<string> result = origin.Split('\r', '\n').ToList<string>();
+            result.RemoveAt(result.Count - 1);
+            return result;
+        }
+
+        public void StockScreen(object data)
+        {
+            StockScreenArgs ssa = (StockScreenArgs)data;
+            ssa.ResultTb.Text = "开始筛选\r\n";
+            List<string> result = new List<string>();
+            StockUrl stockUrl = new StockUrl();
+            string[] headArray = new string[] { "sh600", "sh601", "sh603", "sz000", "sz300" };
+            string EndDate = ssa.EndDate;
+            string BeginDate = ssa.BeginDate;
+            int headIndex = 0;
+            int tailIndex = 0;
+            string stockStr = "";
+            for (headIndex = 0; headIndex < headArray.Length; headIndex++)
+            {
+                for (tailIndex = 1; tailIndex <= 999; tailIndex++)
+                {
+                    stockStr = headArray[headIndex] + tailIndex.ToString("000");
+                    stockUrl.Settings(headArray[headIndex], tailIndex.ToString("000"), BeginDate, EndDate);
+                    string StockOriginData = HttpGet(stockUrl.URL);
+                    List<string> historyStrings = SplitOriginStr(StockOriginData);
+                    List<StockHistory> history = new List<StockHistory>();
+                    for (int i = 0; i < historyStrings.Count; i++)
+                    {
+                        history.Add(new StockHistory(historyStrings[i]));
+                    }
+                    int declineCount = 0;
+                    for (int i = 1; i < history.Count; i++)
+                    {
+                        if (history[i].Close - history[i - 1].Close < 0)
+                        {
+                            declineCount++;
+                        }
+                    }
+                    bool choose = false;
+                    if (declineCount >= ssa.DeclineDayCount)
+                    {
+                        choose = true;
+                    }
+                    if (choose)
+                    {
+                       ssa.ResultTb.AppendText(stockStr + "\r\n");
+                       result.Add(stockStr);
+                    }
+                }
+            }
+            ssa.ResultTb.AppendText("筛选完成！");
+            ssa.SCallback(result);
+        }
+    }
+
+    class StockUrl
+    {
+        string urlHead = "http://biz.finance.sina.com.cn/stock/flash_hq/kline_data.php?&rand=random(10000)&symbol=";
+        string stockHead = "sh000";
+        string stockTail = "001";
+        string urlEndDateHead = "&end_date=";
+        string urlEndDate = "20151203";
+        string urlBeginDateHead = "&begin_date=";
+        string urlBeginDate = "20151201";
+        string urlTail = "&type=plain";
+
+        public string URL { get { return urlHead + stockHead + stockTail + urlEndDateHead + urlEndDate + urlBeginDateHead + urlBeginDate + urlTail; } }
+
+        public StockUrl()
+        {
+
+        }
+
+        public StockUrl(string stockHead, string stockTail, string urlBeginDate, string urlEndDate)
+        {
+            this.stockHead = stockHead;
+            this.stockTail = stockTail;
+            this.urlBeginDate = urlBeginDate;
+            this.urlEndDate = urlEndDate;
+        }
+
+        public void Settings(string stockHead, string stockTail, string urlBeginDate, string urlEndDate)
+        {
+            this.stockHead = stockHead;
+            this.stockTail = stockTail;
+            this.urlBeginDate = urlBeginDate;
+            this.urlEndDate = urlEndDate;
+        }
+    }
+
+    class StockHistory
+    {
+        DateTime dt;
+        float open;
+        float high;
+        float close;
+        float low;
+        int total;
+
+        public DateTime DT { get { return dt; } }
+        public float Open { get { return open; } }
+        public float Hign { get { return high; } }
+        public float Close { get { return close; } }
+        public float Low { get { return low; } }
+        public int Total { get { return total; } }
+
+
+        public StockHistory(string historyStr)
+        {
+            string[] array = historyStr.Split(',');
+            dt = DateTime.Parse(array[0]);
+            open = float.Parse(array[1]);
+            high = float.Parse(array[2]);
+            close = float.Parse(array[3]);
+            low = float.Parse(array[4]);
+            total = int.Parse(array[5]);
+        }
+    }
+}
